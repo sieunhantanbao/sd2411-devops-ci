db8a9c485cd95824a64f4800ee43fc6d36ad80af
HARVEYNASH\anhnguyens <sieunhantanbao@gmail.com>
2023-10-08 06:56:40 +0700

    Update groovy file

---
+++ vars/nodejs.groovy
@@ -0,0 +1,59 @@
+#!/usr/bin/env groovy
+void call() {
+    String backend = "backend"
+    String frontend = "frontend"
+    String dockerRegistry = "acranhnguyens.azurecr.io"
+//========================================================================
+//========================================================================
+
+//========================================================================
+//========================================================================
+
+    // stage ('Copy Dockerfiles to the source') {
+    //     script {
+    //         writeFile file: 'backend/Dockerfile.Backend', text: libraryResource('dev/docker/Dockerfile.Backend')
+    //         writeFile file: 'frontend/Dockerfile.Frontend', text: libraryResource('dev/docker/Dockerfile.Frontend')
+    //     }
+    // }
+
+    stage ("Build Backend") {
+        files = findFiles(glob: '*.*')
+        files.each { file ->
+            echo "File: ${file.toString()} +++"
+        }
+        docker.build("${dockerRegistry}/${backend}:${BUILD_NUMBER}", "--force-rm --no-cache -f ./backend/Dockerfile .")
+    }
+
+    stage ("Build Frontend") {
+        docker.build("${dockerRegistry}/${frontend}:${BUILD_NUMBER}", "--force-rm --no-cache -f ./frontend/Dockerfile .")
+    }
+
+    stage ("Push Docker Images to ACR - backend") {
+        withCredentials([[$class: 'ACRCredential', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+            docker.withRegistry("https://${dockerRegistry}", acrCredential ) {
+                sh "docker login ${dockerRegistry} -u ${USERNAME} -p ${PASSWORD}"
+                sh "docker push ${dockerRegistry}/${backend}/${name}:${BUILD_NUMBER}"
+            }
+        }
+    }
+
+    stage ("Push Docker Images to ACR - frontend") {
+        withCredentials([[$class: 'ACRCredential', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+            docker.withRegistry("https://${dockerRegistry}", acrCredential ) {
+                sh "docker login ${dockerRegistry} -u ${USERNAME} -p ${PASSWORD}"
+                sh "docker push ${dockerRegistry}/${frontend}/${name}:${BUILD_NUMBER}"
+            }
+        }
+    }
+}
+
+//========================================================================
+// Demo CI
+// Version: v1.0
+// Updated:
+//========================================================================
+//========================================================================
+// Notes:
+//
+//
+//========================================================================
--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -1,59 +1,48 @@
 #!/usr/bin/env groovy
 void call(Map pipelineParams) {
-    String backend = "backend"
-    String frontend = "frontend"
-    String dockerRegistry = "acranhnguyens.azurecr.io"
-//========================================================================
-//========================================================================
 
-//========================================================================
-//========================================================================
+    pipeline {
 
-    // stage ('Copy Dockerfiles to the source') {
-    //     script {
-    //         writeFile file: 'backend/Dockerfile.Backend', text: libraryResource('dev/docker/Dockerfile.Backend')
-    //         writeFile file: 'frontend/Dockerfile.Frontend', text: libraryResource('dev/docker/Dockerfile.Frontend')
-    //     }
-    // }
+        agent any
 
-    stage ("Build Backend") {
-        files = findFiles(glob: '*.*')
-        files.each { file ->
-            echo "File: ${file.toString()} +++"
+        options {
+            disableConcurrentBuilds()
+            disableResume()
+            timeout(time: 1, unit: 'HOURS')
         }
-        docker.build("${dockerRegistry}/${backend}:${BUILD_NUMBER}", "--force-rm --no-cache -f ./backend/Dockerfile .")
-    }
-
-    stage ("Build Frontend") {
-        docker.build("${dockerRegistry}/${frontend}:${BUILD_NUMBER}", "--force-rm --no-cache -f ./frontend/Dockerfile .")
-    }
-
-    stage ("Push Docker Images to ACR - backend") {
-        withCredentials([[$class: 'ACRCredential', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            docker.withRegistry("https://${dockerRegistry}", acrCredential ) {
-                sh "docker login ${dockerRegistry} -u ${USERNAME} -p ${PASSWORD}"
-                sh "docker push ${dockerRegistry}/${backend}/${name}:${BUILD_NUMBER}"
+        
+        stages {
+            stage ('Load Pipeline') {
+                when {
+                    allOf {
+                        // Condition Check
+                        anyOf{
+                            // Branch Event: Nornal Flow
+                            anyOf {
+                                branch 'main'
+                                 branch 'master'
+                                branch 'jenkins'
+                                branch 'PR-*'
+                            }
+                            // Manual Run: Only if checked.
+                            allOf{
+                                triggeredBy 'UserIdCause'
+                            }
+                        }
+                    }
+                }
+                steps {
+                    script {
+                        nodejs()
+                    }
+                }
             }
         }
-    }
 
-    stage ("Push Docker Images to ACR - frontend") {
-        withCredentials([[$class: 'ACRCredential', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            docker.withRegistry("https://${dockerRegistry}", acrCredential ) {
-                sh "docker login ${dockerRegistry} -u ${USERNAME} -p ${PASSWORD}"
-                sh "docker push ${dockerRegistry}/${frontend}/${name}:${BUILD_NUMBER}"
-            }
-        }
+        // post {
+        //     cleanup {
+        //         cleanWs()
+        //     }
+        // }
     }
 }
-
-//========================================================================
-// Demo CI
-// Version: v1.0
-// Updated:
-//========================================================================
-//========================================================================
-// Notes:
-//
-//
-//========================================================================
